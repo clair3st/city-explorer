6344a08ba8503eb1f4ef3e813d3d1ef4e4b2636e
clair3st <clairejgatenby@gmail.com>
2024-10-18 17:48:58 -0700

    added a static map

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -9,32 +9,42 @@ function Main() {
 	const [searchInput, setSearchInput] = useState('');
 	const [location, setLocation] = useState({})
 
+
 	function handleSubmit(e) {
 		e.preventDefault();
 
 		axios.get(`https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${searchInput}&format=json`)
 			.then(response =>{
-				console.log('success ', response.data)
+				console.log('success data', response.data)
 				if (response.data.length){
-					setLocation({lat: response.data[0].lat, lon: response.data[0].lon, display_name: response.data[0].display_name})				
-				}
+					setLocation({lat: response.data[0].lat, lon: response.data[0].lon, display_name: response.data[0].display_name})			
+				}	
 
 			})
+
 
 	}
 
 	function Location(location){
 		
 		if('lon' in location){
+			
 			return(
 				<div>
 				<p> {location.display_name}</p>
 				<p> {location.lat}, {location.lon}</p>
+				<img
+            		src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${location.lat},${location.lon}&zoom=12&size=600x400&format=jpg&maptype=streets`}
+           			 alt='map'
+          		/>
 				</div>
+
 			)
 
 		} 
 	}
+
+
 
 	return(
 		<div>
